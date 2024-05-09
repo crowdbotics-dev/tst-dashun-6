add994f6bd9e09c226c83ed45b2eb5286757d10d
Crowdbotics <engineering@crowdbotics.com>
2024-05-09 19:44:33 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tst-dashun-6",
+  url: "https://tst-dashun-6.botics.co",
+  api: "https://tst-dashun-6.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
